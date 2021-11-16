17918895ee50e9d6ab452ccf3d8e402749f0924e
Postman47 <piotr2001@gmail.com>
2021-11-16 19:59:06 +0100

    Api project

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student piotr = new Student(
+                    "Piotr",
+                    "piotr789@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 4)
+            );
+
+            Student jan = new Student(
+                    "Jan",
+                    "jan789@gmail.com",
+                    LocalDate.of(2002, Month.JANUARY, 12)
+            );
+
+            repository.saveAll(
+                    List.of(piotr, jan)
+            );
+        };
+    }
+}
