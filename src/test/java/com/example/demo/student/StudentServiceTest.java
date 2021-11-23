1c7d26845865461d43aa4141014a1e274bc3ad99
Postman47 <piotr2001@gmail.com>
2021-11-23 19:13:55 +0100

    Added @Test canGetAllStudents

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -5,13 +5,18 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -34,16 +39,44 @@ class StudentServiceTest {
     }
 
     @Test
-
-    void canAddNewStudent() throws EmailTakenException{
+    void canAddNewStudent() {
         //given
         Student student = new Student(
                 "Hanna",
                 "hanna23@gmail.com",
                 LocalDate.of(1993, Month.DECEMBER, 17)
         );
+
         //when
         underTest.addNewStudent(student);
+
+        //then
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+        Student capturedStudent = studentArgumentCaptor.getValue();
+        assertThat(capturedStudent).isEqualTo(student);
+    }
+
+    @Test
+    void willThrowWhenEmailIsTaken() {
+        //given
+        Student student = new Student(
+                "Hanna",
+                "hanna23@gmail.com",
+                LocalDate.of(1993, Month.DECEMBER, 17)
+        );
+
+
+        //TODO: test if thrown exception work
+        //when
+//        underTest.addNewStudent(student);
+//        Optional <Student> testStudent = studentRepository.findStudentByEmail(student.getEmail());
+//        given(testStudent.isPresent()).willReturn(true);
+//
+//
+//        //then
+//        assertThatThrownBy(() -> underTest.addNewStudent(student)).hasMessageContaining(EmailTakenException.getErrorEmailTaken());
+
     }
 
     @Test
